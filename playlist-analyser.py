191ace1459b9ee404480f547c4d9ee815cd0e21a
Olie Ryan-George <23618262+occrg@users.noreply.github.com>
2025-07-30 22:22:39 +0100

    refactored to only use env variables in one place and only call spotify auth endpoint once

--- playlist-analyser.py
+++ playlist-analyser.py
@@ -2,18 +2,13 @@ import json
 import requests
 from itertools import repeat
 
-with open("variables.json", "r") as file:
-    env_vars = file.read()
-
-env_vars_json = json.loads(env_vars)
-
-def get_spotify_auth_header():
+def get_spotify_access_token(spotify_auth):
     url = "https://accounts.spotify.com/api/token"
 
     auth_form_data = {
         "grant_type": "client_credentials",
-        "client_id": env_vars_json["spotify_auth"]["client_id"],
-        "client_secret": env_vars_json["spotify_auth"]["client_secret"]
+        "client_id": spotify_auth["client_id"],
+        "client_secret": spotify_auth["client_secret"]
     }
 
     auth_response = requests.post(url, data=auth_form_data)
@@ -21,13 +16,13 @@ def get_spotify_auth_header():
 
     return access_token
 
-def get_spotify_playlist_tracks(playlist_id):
+def get_spotify_playlist_tracks(spotify_access_token, playlist_id):
     spotify_url = "https://api.spotify.com/v1/"
     playlist_endpoint = "playlists/"
     playlist_url = spotify_url + playlist_endpoint + playlist_id
 
     standard_request_header = {
-        "Authorization": f"Bearer {get_spotify_auth_header()}"
+        "Authorization": f"Bearer {spotify_access_token}"
     }
     playlist_request_parameters = {
         "fields": "name,tracks.items(track(id,name,artists(id,name)))"
@@ -41,13 +36,23 @@ def flatten_song_data(playlist_track_data, playlist_name):
     playlist_track_data.update({"playlist_name": playlist_name})
     return playlist_track_data
 
-def create_tracklist_for_all_spotify_playlists():
+def create_tracklist_for_all_spotify_playlists(spotify_access_token, playlist_ids):
     tracklist_for_all_spotify_playlists = []
-    for playlist_id in env_vars_json["playlist_ids"]:
-        playlist_data = get_spotify_playlist_tracks(playlist_id)
+    for playlist_id in playlist_ids:
+        playlist_data = get_spotify_playlist_tracks(spotify_access_token, playlist_id)
         playlist_track_data = playlist_data["tracks"]["items"]
         playlist_flat_track_data = list(map(flatten_song_data, playlist_track_data, repeat(playlist_data["name"])))
         tracklist_for_all_spotify_playlists.extend(playlist_flat_track_data)
     print(json.dumps(tracklist_for_all_spotify_playlists))
 
-create_tracklist_for_all_spotify_playlists()
+
+def run_script():
+    with open("variables.json", "r") as file:
+        env_vars = file.read()
+    
+    env_vars_json = json.loads(env_vars)
+
+    spotify_access_token = get_spotify_access_token(env_vars_json["spotify_auth"])
+    create_tracklist_for_all_spotify_playlists(spotify_access_token, env_vars_json["playlist_ids"])
+
+run_script()

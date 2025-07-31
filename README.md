f4ddeacff4c90316617eb01d8e5907c6c1024079
Olie Ryan-George <23618262+occrg@users.noreply.github.com>
2025-07-31 08:46:57 +0100

    track analysis API called for track qualities

--- README.md
+++ README.md
@@ -3,10 +3,9 @@
 A tool that analyses the songs in Spotify playlists and how the features of the songs differ in each.
 
 ## Current functionality
-The application currently takes a list of Spotify playlists and returns the Spotify IDs, artists and names of the songs within each playlists.
+The application currently takes a list of Spotify playlists and returns a flat list of tracks with their names, artists and qualities from the [Track Analysis API](https://rapidapi.com/soundnet-soundnet-default/api/track-analysis/playground/apiendpoint_78b81b32-03a1-4044-aa46-ac17aa2528fe).
 
 ## Future functionality
-1. The application should call the [Track Analysis API](https://rapidapi.com/soundnet-soundnet-default/api/track-analysis/playground/apiendpoint_78b81b32-03a1-4044-aa46-ac17aa2528fe) to get qualities of each track.
 1. Each playlist will have their tracks' qualities aggregated.
 1. The playlists aggregated qualities will be visualised.
 
@@ -17,6 +16,14 @@ The application currently takes a list of Spotify playlists and returns the Spot
 * You have [made a Spotify API application](https://developer.spotify.com/documentation/web-api/concepts/apps) that can access the Spotify Web API.
 
 ## Instructions
-1. Copy the `variables.example.json` file and give it the name `vairables.json` and replace the values with your own values.
+1. Copy the `variables.example.json` file and give it the name `variables.json` and replace the values with your own values.
 1. Run the `playlist-analyser.py` file.
+
+## To Do
+This is a list of things to do for the project, not including implementing the "future functionality" above.
+1. Store data from playlist analyser API to avoid hitting API limit.
+1. Add Spotify popularity and duration to qualities.
+1. Check whether happiness is the same as valence between different APIs.
+1. Ensure global constants are kept separately.
+1. Add error handling for API calls.
 
--- playlist-analyser.py
+++ playlist-analyser.py
@@ -1,6 +1,7 @@
 import json
 import requests
 from itertools import repeat
+import time
 
 def get_spotify_access_token(spotify_auth):
     url = "https://accounts.spotify.com/api/token"
@@ -43,8 +44,45 @@ def create_tracklist_for_all_spotify_playlists(spotify_access_token, playlist_id
         playlist_track_data = playlist_data["tracks"]["items"]
         playlist_flat_track_data = list(map(flatten_song_data, playlist_track_data, repeat(playlist_data["name"])))
         tracklist_for_all_spotify_playlists.extend(playlist_flat_track_data)
-    print(json.dumps(tracklist_for_all_spotify_playlists))
+    return tracklist_for_all_spotify_playlists
 
+def get_track_qualities(track_analysis_auth, track):
+    track_analysis_url = "https://track-analysis.p.rapidapi.com/pktx/"
+    track_analysis_endpoint = "spotify/"
+    track_url = track_analysis_url + track_analysis_endpoint + track["track"]["id"]
+    
+    standard_request_header = {
+        "x-rapidapi-key": track_analysis_auth["api_key"]
+    }
+
+    track_analysis_response = requests.get(track_url, headers=standard_request_header)
+
+    time.sleep(2)
+
+    return track_analysis_response.json()
+
+
+def add_track_quality_to_tracklist(track_analysis_auth, track):
+    track_qualities = get_track_qualities(track_analysis_auth, track)
+    
+    track.update({
+        "track_qualities": {
+            "key": track_qualities["key"],
+            "mode": track_qualities["mode"],
+            "tempo": track_qualities["tempo"],
+            "popularity": track_qualities["popularity"],
+            "energy": track_qualities["energy"],
+            "danceability": track_qualities["danceability"],
+            "happiness": track_qualities["happiness"],
+            "acousticness": track_qualities["acousticness"],
+            "instrumentalness": track_qualities["instrumentalness"],
+            "liveness": track_qualities["liveness"],
+            "speechiness": track_qualities["speechiness"],
+            "loudness": track_qualities["loudness"]
+        }
+    })
+
+    return track
 
 def run_script():
     with open("variables.json", "r") as file:
@@ -53,6 +91,9 @@ def run_script():
     env_vars_json = json.loads(env_vars)
 
     spotify_access_token = get_spotify_access_token(env_vars_json["spotify_auth"])
-    create_tracklist_for_all_spotify_playlists(spotify_access_token, env_vars_json["playlist_ids"])
+    tracklist_for_all_playlists = create_tracklist_for_all_spotify_playlists(spotify_access_token, env_vars_json["playlist_ids"])
+    tracklist_for_all_playlists_with_track_qualities = list(map(add_track_quality_to_tracklist, repeat(env_vars_json["x_rapid_auth"]), tracklist_for_all_playlists))
+
+    print(json.dumps(tracklist_for_all_playlists_with_track_qualities))
 
 run_script()
